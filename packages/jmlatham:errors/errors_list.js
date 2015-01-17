eeafc2331d781443ef9b8aa1d2a93b2ac78d9ff4
Marshall Latham <jmlatham@yahoo.com>
2015-01-16 23:40:27 -0600

    201501162340
    Commit 9.5.0
    before the delete of good files

---
+++ packages/jmlatham:errors/errors_list.js
@@ -0,0 +1,15 @@
+Template.meteorErrors.helpers({ 
+  errors: function() {
+	return Errors.collection.find(); 
+  }
+});
+
+Template.meteorError.rendered = function() { 
+  var error = this.data; 
+  Meteor.setTimeout(
+	function () {
+		Errors.collection.remove(error._id);
+	}, 
+	3000
+  );
+};
